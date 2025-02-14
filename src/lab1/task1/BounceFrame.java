ca7608defde11fbff91ca97a867b6560cb6d7eae
sana23ok <oksanahordych@gmail.com>
2025-02-14 19:49:30 +0200

    gui changes

--- src/lab1/task1/BounceFrame.java
+++ src/lab1/task1/BounceFrame.java
@@ -9,44 +9,67 @@ public class BounceFrame extends JFrame {
     private BallCanvas canvas;
     public static final int WIDTH = 450;
     public static final int HEIGHT = 350;
+
     public BounceFrame() {
         this.setSize(WIDTH, HEIGHT);
-        this.setTitle("lab1.task1. Bounce program");
+        this.setTitle("task1. Bounce program");
         this.canvas = new BallCanvas();
-        System.out.println("In Frame Thread name = "
-                + Thread.currentThread().getName());
+        System.out.println("In Frame Thread name = " + Thread.currentThread().getName());
         Container content = this.getContentPane();
         content.add(this.canvas, BorderLayout.CENTER);
+
         JPanel buttonPanel = new JPanel();
         buttonPanel.setBackground(Color.lightGray);
-        JButton buttonStart = new JButton("Start");
-        JButton buttonStop = new JButton("Stop");
-        buttonStart.addActionListener(new ActionListener() {
 
+        JButton buttonAdd1 = new JButton("Add 1");
+        JButton buttonAdd10 = new JButton("Add 10");
+        JButton buttonAdd100 = new JButton("Add 100");
+        JButton buttonStop = new JButton("Stop");
+
+        buttonAdd1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                Ball b = new Ball(canvas);
-                canvas.add(b);
-
-                BallThread thread = new BallThread(b);
-                thread.start();
-                System.out.println("Thread name = " +
-                        thread.getName());
+                addBalls(1);
             }
         });
+
+        buttonAdd10.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addBalls(10);
+            }
+        });
+
+        buttonAdd100.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addBalls(100);
+            }
+        });
+
         buttonStop.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
                 System.exit(0);
             }
         });
 
-
-        buttonPanel.add(buttonStart);
+        buttonPanel.add(buttonAdd1);
+        buttonPanel.add(buttonAdd10);
+        buttonPanel.add(buttonAdd100);
         buttonPanel.add(buttonStop);
 
         content.add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    private void addBalls(int count) {
+        for (int i = 0; i < count; i++) {
+            Ball b = new Ball(canvas);
+            canvas.add(b);
+
+            BallThread thread = new BallThread(b);
+            thread.start();
+            System.out.println("Thread name = " + thread.getName());
+        }
     }
 }
--- src/lab1/task2/BounceFrame.java
+++ src/lab1/task2/BounceFrame.java
@@ -11,31 +11,46 @@ public class BounceFrame extends JFrame {
 
     public BounceFrame() {
         this.setSize(WIDTH, HEIGHT);
-        this.setTitle("lab1.task2. Billiard Simulation.");
+        this.setTitle("task2. Billiard Simulation.");
         this.canvas = new BallCanvas();
         scoreLabel = new JLabel("Score: 0");
 
-        System.out.println("In Frame Thread name = "
-                + Thread.currentThread().getName());
+        System.out.println("In Frame Thread name = " + Thread.currentThread().getName());
         Container content = this.getContentPane();
         content.add(this.canvas, BorderLayout.CENTER);
+
         JPanel buttonPanel = new JPanel();
         buttonPanel.setBackground(Color.lightGray);
-        JButton buttonStart = new JButton("Start");
+
+        JButton buttonAdd1 = new JButton("Add 1");
+        JButton buttonAdd10 = new JButton("Add 10");
+        JButton buttonAdd100 = new JButton("Add 100");
         JButton buttonStop = new JButton("Stop");
 
-        buttonStart.addActionListener(e -> {
-            Ball ball = new Ball(canvas);
-            canvas.add(ball);
-            BallThread thread = new BallThread(ball, canvas, scoreLabel);
-            thread.start();
-        });
+        buttonAdd1.addActionListener(e -> addBalls(1));
+        buttonAdd10.addActionListener(e -> addBalls(10));
+        buttonAdd100.addActionListener(e -> addBalls(100));
 
         buttonStop.addActionListener(e -> System.exit(0));
 
-        buttonPanel.add(buttonStart);
+        buttonPanel.add(buttonAdd1);
+        buttonPanel.add(buttonAdd10);
+        buttonPanel.add(buttonAdd100);
         buttonPanel.add(buttonStop);
         buttonPanel.add(scoreLabel);
+
         content.add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    private void addBalls(int count) {
+        for (int i = 0; i < count; i++) {
+            Ball ball = new Ball(canvas);
+            canvas.add(ball);
+
+            BallThread thread = new BallThread(ball, canvas, scoreLabel);
+            thread.start();
+
+            System.out.println("Thread name = " + thread.getName());
+        }
     }
 }
